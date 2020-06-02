68593c49f9716fb17ea9595a0a10d0218d0bf949
Mehmet Nuri Yılmaz <mehmetnurii48@gmail.com>
2020-06-03 02:16:06 +0300

    Currency class'ı eklendi

---
+++ Currency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NesneFinal
+{
+    class Currency
+    {
+        private Dictionary<string, double> Currencies = new Dictionary<string, double>();
+       
+
+        public Currency()
+        {
+            Currencies.Add("TRY/USD", 3.0);
+            Currencies.Add("TRY/EURO", 3.0);
+            Currencies.Add("USD/TRY", 3.0);
+            Currencies.Add("USD/EURO", 3.0);
+            Currencies.Add("EURO/TRY", 3.0);
+            Currencies.Add("EURO/USD", 3.0);
+
+        }
+    }
+}
