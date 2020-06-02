d649616caa4eb09e55f2e32360c01d52e4378c0c
Mehmet Nuri Yılmaz <mehmetnurii48@gmail.com>
2020-06-03 01:15:34 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NesneFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
